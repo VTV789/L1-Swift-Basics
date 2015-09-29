c8b0941e0522ed32048aa68b9017a1d35244d15c
Vinh Thanh Vu <vtv@vinh.com>
2015-09-28 23:18:58 -0400

    First Commit - Swift Basics

---
+++ L1_Types&Operators.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+var str = "Hello, playground"
+
+
+// Example 1: Bool, Int Float, Double
+class LightSwitch {
+    var on: Bool = true
+    var dimmer: Double = 3.14159265359
+}
+
+var livingRoomSwitch = LightSwitch()
+livingRoomSwitch.on
+
