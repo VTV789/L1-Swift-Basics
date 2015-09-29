ff41498642354a2dfb897f92dca19a21dd39f8e0
Vinh Thanh Vu <vtv@vinh.com>
2015-09-28 23:39:31 -0400

    Comparison Operators

--- L1_Types&Operators.playground/Contents.swift
+++ L1_Types&Operators.playground/Contents.swift
@@ -20,8 +20,19 @@ var dollarSign = "4"
 var myFirstSwiftString = "mo' money"
 var mySecondSwiftString: String = "mo' problems"
 var concatenatedString = myFirstSwiftString + ", " + mySecondSwiftString
-
+concatenatedString.dynamicType
 
 // Stay tuned for more on Optionals and Tuples in the upcoming lessons!
 
-// Operators 
+// OPERATORS: Example 1 - Comparison operators 
+let ticketPrice = 7.5
+let allowance = 10.0
+var iceCreamPrice = 3.0
+
+//var pic = UIImage(named: "Choloe.png")!
+
+if allowance >= ticketPrice + iceCreamPrice {
+    print("Let's go to the movies!")
+} else {
+    print("Let's watch a movie at home and eat ice cream")
+}
