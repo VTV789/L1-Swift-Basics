08f9dcfc83a9cb64ce2f772f67e45c71fbc002c7
Vinh Thanh Vu <vtv@vinh.com>
2015-10-01 23:25:08 -0400

    Constants and variables: Example 2a

--- L1_Constrants&Variables.playground/Contents.swift
+++ L1_Constrants&Variables.playground/Contents.swift
@@ -9,3 +9,8 @@ let encouragement = "You can do it!"
 /* Use var when you expect the value to change */
 var personalizedEncouragement = "You can do it!"
 personalizedEncouragement = personalizedEncouragement.stringByReplacingOccurrencesOfString("Lauren", withString: "Cameron")
+
+// Example 2a
+let birthYear = 2008
+var currentYear = 2015
+var age = currentYear - birthYear
